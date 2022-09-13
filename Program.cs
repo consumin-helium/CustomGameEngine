9bc33a9298edf08cf643d711846cefa21751f61b
ConsuminHelium <rednaxelanosbor@gmail.com>
2022-09-13 17:45:54 +0200

    First Engine Build

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿string output = "Placeholder";
+
+
+#region Test Calculations
+
+
+void CreatePoint(){
+    // Create a new vertex point that defaults to 0,0,0
+    var emptyVertex = new int[][] {
+        new int[]{0,0,0},
+        new int[]{0,0,0},
+        new int[]{1,1,1}
+    };
+    return emptyVertex;
+}
+
+#endregion
+
+Console.WriteLine("Hello, World!");
+var test = CreatePoint();
+
+Console.WriteLine(output);
